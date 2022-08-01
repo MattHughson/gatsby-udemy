3f5968164b3a303780c3f2bc0aef05bf1a5b29ed
Matt Hughson <matthew.hughson@lesappelsillimites.com>
2022-08-01 13:21:21 -0400

    test

---
+++ src/pages/company/history.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const history = () => {
+  return (
+    <div>
+      <h1>This is the company history</h1>
+    </div>
+  )
+}
+
+export default history
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div>Hello world!</div>
+    </div>
+  )
+}
